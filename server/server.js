5ddbcbaa32d64d3a0c6fc74b4261bb8d72d53c1b
pushpam patel <30745430+pushpam-patel@users.noreply.github.com>
2019-09-26 23:56:39 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -6,19 +6,33 @@ let cors = require('cors')
 
 let {mongoose} = require ('./db/mongoose.js')
 
-let {User} = require('./modedls/user.js')
-let {TrafficCom} = require('./model/trafficcomp.js')
-let {AccidentCom} = require('./model/accidentcomp.js')
-let { DailySchedules } = require('./models/dailyschedules.js')
+let {User} = require('./models/user.js')
+let {TrafficCom} = require('./models/trafficcomp.js')
+let {AccidentCom} = require('./models/accidentcomp.js')
+let { DailySchedules } = require('./models/dailyschedule.js')
 let {PoliceUser} = require('./models/police.js')
 let {HospitalUser}= require('./models/hospital.js')
 
 let app = express()
 let port = process.env.PORT || 3000
 
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 app.use(cors())
 
+app.post('/rgeo',(req,res)=>{
+    let lat=req.body.lat
+    let long=req.body.long
+    
+    let config={
+        'latitude':lat,
+        'longitude':long,
+        'key':'AIzaSyCEF1MgrRBaktN47f3Xf_sb1POSHnDunY0'
+    }
+
+    geo.location(config, function (err, data){
+        res.send(data.results[0].formatted_address)
+    });
+})
 
 app.get('/users', (req, res) => {
     User.find().then((users) => {
@@ -71,6 +85,16 @@ app.post('/traffic', (req, res) => {
     })
 })
 
+
+app.get('/accident', (req, res) => {
+    AccidentCom.find().then((accident) => {
+        res.send(accident)
+    }).catch((err) => {
+        res.status(400).send(err)
+    })
+})
+
+
 app.post('/accident', (req, res) => {
     console.log(req.body)
 
@@ -96,3 +120,19 @@ app.get('/daily', (req, res) => {
     })
 })
 
+app.post('/daily', (req, res) => {
+    let daily = new DailySchedules({
+        date:req.body.date,
+        bus:req.body.bus,
+        train:req.body.train,
+        metro:req.body.metro
+    })
+
+    daily.save().then((doc) => {
+        res.send(doc)
+    }).catch((err) => {
+        res.status(400).send(err)
+    })
+})
+
+app.listen(port)
